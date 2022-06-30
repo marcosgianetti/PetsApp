4c9077868288bf958b4e2c3fc18effea7a69f3e9
Marcos Gianetti <marcos.gianetti@carone.com.br>
2022-06-29 22:01:29 -0300

    teste tela profile

--- pets_app/test/pages/profile/profile_test.dart
+++ pets_app/test/pages/profile/profile_test.dart
@@ -1,20 +1,41 @@
+import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:pets_app/domain/entities/entities.dart';
 import 'package:pets_app/ui/pages/profile/profile.dart';
 
 void main() {
-  group('Profile Screen: ', () {
-    testWidgets('Should show circualar progress indicator on init screen', (WidgetTester tester) async {
-      await tester.pumpWidget(MaterialApp(home: ProfileScreen()));
-      final circularProgressIndicator = find.byType(CircularProgressIndicator);
+  group(
+    'Profile Screen: ',
+    () {
+      testWidgets('Should show circualar progress indicator on init screen', (WidgetTester tester) async {
+        await tester.pumpWidget(MaterialApp(home: ProfileScreen()));
+        final circularProgressIndicator = find.byType(CircularProgressIndicator);
 
-      expect(circularProgressIndicator, findsOneWidget);
-    });
-    testWidgets('Should show "Pagina de perfil" while api don\'t response', (WidgetTester tester) async {
-      await tester.pumpWidget(MaterialApp(home: ProfileScreen()));
-      final appBarTitle = find.text('Pagina de perfil');
+        expect(circularProgressIndicator, findsOneWidget);
+      });
+      testWidgets(
+        'Should show "Pagina de perfil" while api don\'t response',
+        (WidgetTester tester) async {
+          String fakeId = faker.guid.guid();
+          String name = faker.internet.email();
+          await tester.pumpWidget(
+            MaterialApp(
+              onGenerateRoute: (settings) {
+                return MaterialPageRoute(
+                  settings: RouteSettings(arguments: [fakeId, PetType.dog, name]),
+                  builder: (context) {
+                    return ProfileScreen();
+                  },
+                );
+              },
+            ),
+          );
+          final appBarTitle = find.text('Cão - $name');
 
-      expect(appBarTitle, findsOneWidget);
-    });
-  });
+          expect(appBarTitle, findsOneWidget);
+        },
+      );
+    },
+  );
 }
